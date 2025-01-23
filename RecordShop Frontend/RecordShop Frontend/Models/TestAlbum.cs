01f85ebaa2074530831723cc96e0cd40f5a8d26d
stripi <cgm998cgm@gmail.com>
2025-01-23 16:51:13 +0000

    Implement Frontend display

---
+++ RecordShop Frontend/RecordShop Frontend/Models/TestAlbum.cs
@@ -0,0 +1,11 @@
+﻿using RecordShop;
+
+public class TestAlbum
+{
+    public static Album testAlbum1 = new Album { Id = 1, Name = "Test Album", Artist = "TestMan", Genre = "LOFI", Year = 1999, Stock = 1 };
+    public static Album testAlbum2 = new Album { Id = 2, Name = "Test Album The Sequel", Artist = "TestMan's Brother", Genre = "LOFI", Year = 1999, Stock = 0 };
+
+    public static List<Album> albumList = new List<Album>() { testAlbum1, testAlbum2 };
+    
+}
+
